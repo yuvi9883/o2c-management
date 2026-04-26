1f833e13094294d46b31f3a84f6cb6410f8f884f
Yuvi9883 <koiladayuvaraj9860@gmail.com>
2026-04-26 20:43:10 +0530

    prepare for netlify deployment

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router':       ['react-router-dom'],
+          'charts':       ['recharts'],
+          'axios':        ['axios'],
+        },
+      },
+    },
+  },
 })
